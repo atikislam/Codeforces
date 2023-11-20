92c06a9674249b24e40f6c12dd560da00b5e9966
atikislam <atikislam834@gmail.com>
2023-11-20 15:43:54 +0600

    D Same Differencep

---
+++ D_Same_Differences.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define abc "abcdefghijklmnopqrstuvwxyz"
+#define asort(a) sort(a.begin(),a.end())
+#define gsort(a) sort(a.begin(),a.end(),greater<int>())
+#define issort(a) is_sorted(a.begin(),a.end())
+#define areverse(a) reverse(a.begin(),a.end())
+#define Faster ios_base::sync_with_stdio(false); cin.tie(NULL);
+// default define end
+void solve(){
+    ll n,total_pair=0;
+    cin>>n;
+    map<int,int>freq;
+    for(int i=1;i<=n;i++){
+       int x;cin>>x;
+       x-=i;
+       total_pair+=freq[x];
+       freq[x]++;
+    }
+    cout<<total_pair<<endl;
+}
+int main() {
+    Faster;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
